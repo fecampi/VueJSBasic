1303b397e8a57ec70c2fdeb1bea3c5e18b81a1f4
fecampi <felipe.campinho@hotmail.com>
2020-11-30 22:55:52 -0300

    SlideYCssTransition

--- 09-Transições/src/App.vue
+++ 09-Transições/src/App.vue
@@ -1,25 +1,30 @@
 <template>
-	<div id="app" class="container-fluid">
-		<h1>Animações</h1>
-		<hr>
-		<b-button variant="primary" class="mb-4"
-			@click="exibir = !exibir">Mostrar Mensagem</b-button>
-		
-		<FadeCssTransition>
-			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
-		</FadeCssTransition>
+  <div id="app" class="container-fluid">
+    <h1>Animações</h1>
+    <hr />
+    <b-button variant="primary" class="mb-4" @click="exibir = !exibir"
+      >Mostrar Mensagem</b-button
+    >
 
-		<SlideYCssTransition>
-			<b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>
-		</SlideYCssTransition>
+    <FadeCssTransition>
+      <b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
+    </FadeCssTransition>
 
-		<!--<transition 
+    <SlideXCssTransition>
+      <b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>
+    </SlideXCssTransition>
+
+    <SlideYCssTransition>
+      <b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>
+    </SlideYCssTransition>
+
+    <!--<transition 
 			enter-active-class="animated bounce"
 			leave-active-class="animated shake">
 			<b-alert variant="info" show v-show="exibir">{{ msg }}</b-alert>
 		</transition> -->
 
-		<!-- <hr>
+    <!-- <hr>
 		<b-select v-model="tipoAnimacao" class="mb-4">
 			<option value="fade">Fade</option>
 			<option value="slide">Slide</option>
@@ -57,104 +62,109 @@
 			<component :is="componenteSelecionado"></component>
 		</transition> -->
 
-		<hr>
-		<b-button @click="adicionarAluno" class="mb-4">Adicionar Aluno</b-button>
-		<transition-group name="slide" tag="div">
-			<b-list-group v-for="(aluno, i) in alunos" :key="aluno">
-				<b-list-group-item @click="removerAluno(i)">{{ aluno }}</b-list-group-item>
-			</b-list-group>
-		</transition-group>
-	</div>
+    <hr />
+    <b-button @click="adicionarAluno" class="mb-4">Adicionar Aluno</b-button>
+    <transition-group name="slide" tag="div">
+      <b-list-group v-for="(aluno, i) in alunos" :key="aluno">
+        <b-list-group-item @click="removerAluno(i)">{{
+          aluno
+        }}</b-list-group-item>
+      </b-list-group>
+    </transition-group>
+  </div>
 </template>
 
 <script>
-import AlertaAdvertencia from './AlertaAdvertencia.vue'
-import AlertaInfo from './AlertaInfo.vue'
-import FadeCssTransition from './transitions/FadeCssTransition'
-import SlideYCssTransition from './transitions/SlideYCssTransition'
-
+import AlertaAdvertencia from "./AlertaAdvertencia.vue";
+import AlertaInfo from "./AlertaInfo.vue";
+import FadeCssTransition from "./transitions/FadeCssTransition";
+import SlideYCssTransition from "./transitions/SlideYCssTransition";
+import SlideXCssTransition from "./transitions/SlideXCssTransition";
 export default {
-	components: { AlertaAdvertencia, AlertaInfo, FadeCssTransition, SlideYCssTransition },
-	data() {
-		return {
-			alunos: ['Roberto', 'Julia', 'Teresa', 'Paulo'],
-			msg: 'Uma mensagem de informação para o usuário!',
-			exibir: false,
-			exibir2: true,
-			tipoAnimacao: 'fade',
-			larguraBase: 0,
-			componenteSelecionado: 'AlertaInfo'
-		}
-	},
-	methods: {
-		adicionarAluno() {
-			const s = Math.random().toString(36).substring(2)
-			this.alunos.push(s)
-		},
-		removerAluno(indice) {
-			this.alunos.splice(indice, 1)
-		},
-		animar(el, done, negativo) {
-			let rodada = 1
-			const temporizador = setInterval(() => {
-				const novaLargura = this.larguraBase + 
-					(negativo ? -rodada * 10 : rodada * 10)
-				el.style.width = `${novaLargura}px`
-				rodada++
-				if(rodada > 30) {
-					clearInterval(temporizador)
-					done()
-				}
-			}, 20)
-		},
-		beforeEnter(el) {
-			this.larguraBase = 0
-			el.style.width = `${this.larguraBase}px`
-		},
-		enter(el, done) {
-			this.animar(el, done, false)
-		},
-		// afterEnter(el) {
-		// 	console.log('afterEnter')
-		// },
-		// enterCancelled() {
-		// 	console.log('enterCancelled')
-		// },
-		beforeLeave(el) {
-			this.larguraBase = 300
-			el.style.width = `${this.larguraBase}px`
-		},
-		leave(el, done) {
-			this.animar(el, done, true)
-		},
-		// afterLeave(el) {
-		// 	console.log('afterLeave')
-		// },
-		// leaveCancelled() {
-		// 	console.log('enterCancelled')
-		// },
-	}
-}
+  components: {
+    AlertaAdvertencia,
+    AlertaInfo,
+    FadeCssTransition,
+	SlideYCssTransition,
+	SlideXCssTransition,
+  },
+  data() {
+    return {
+      alunos: ["Roberto", "Julia", "Teresa", "Paulo"],
+      msg: "Uma mensagem de informação para o usuário!",
+      exibir: false,
+      exibir2: true,
+      tipoAnimacao: "fade",
+      larguraBase: 0,
+      componenteSelecionado: "AlertaInfo",
+    };
+  },
+  methods: {
+    adicionarAluno() {
+      const s = Math.random().toString(36).substring(2);
+      this.alunos.push(s);
+    },
+    removerAluno(indice) {
+      this.alunos.splice(indice, 1);
+    },
+    animar(el, done, negativo) {
+      let rodada = 1;
+      const temporizador = setInterval(() => {
+        const novaLargura =
+          this.larguraBase + (negativo ? -rodada * 10 : rodada * 10);
+        el.style.width = `${novaLargura}px`;
+        rodada++;
+        if (rodada > 30) {
+          clearInterval(temporizador);
+          done();
+        }
+      }, 20);
+    },
+    beforeEnter(el) {
+      this.larguraBase = 0;
+      el.style.width = `${this.larguraBase}px`;
+    },
+    enter(el, done) {
+      this.animar(el, done, false);
+    },
+    // afterEnter(el) {
+    // 	console.log('afterEnter')
+    // },
+    // enterCancelled() {
+    // 	console.log('enterCancelled')
+    // },
+    beforeLeave(el) {
+      this.larguraBase = 300;
+      el.style.width = `${this.larguraBase}px`;
+    },
+    leave(el, done) {
+      this.animar(el, done, true);
+    },
+    // afterLeave(el) {
+    // 	console.log('afterLeave')
+    // },
+    // leaveCancelled() {
+    // 	console.log('enterCancelled')
+    // },
+  },
+};
 </script>
 
 <style>
 #app {
-	font-family: 'Avenir', Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
-	font-size: 1.5rem;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  font-size: 1.5rem;
 }
 
 .caixa {
-	height: 100px;
-	width: 300px;
-	margin: 30px auto;
-	background-color: lightgreen;
+  height: 100px;
+  width: 300px;
+  margin: 30px auto;
+  background-color: lightgreen;
 }
-
-
-
 </style>
---
+++ 09-Transições/src/transitions/SlideXCssTransition.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+  <transition name="slide" appear>
+    <slot/>
+  </transition>
+  </div>
+</template>
+
+
+<style scoped>
+
+@keyframes slide-in {
+	from { transform: translateX(-400px); }
+	to { transform: translateX(0); }
+}
+
+@keyframes slide-out {
+	from { transform: translateX(0); }
+	to { transform: translateX(-400px); }
+}
+
+.slide-enter-active {
+	animation: slide-in 3s ease;
+	transition: opacity 3s;
+}
+
+.slide-leave-active {
+	position: absolute;
+	width: 100%;
+	animation: slide-out 3s ease;
+	transition: opacity 3s;
+}
+
+.slide-enter, .slide-leave-to {
+	opacity: 0;
+}
+
+.slide-move {
+	transition: transform 1s;
+}
+</style>
